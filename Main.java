f32ab209bafbf3d6c9b6261e0cede59f1d432862
Tony Vang <97489898+TonyVang559@users.noreply.github.com>
2024-01-15 10:18:50 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+            int num1 = 10;
+            int num2 = 5;
+
+            int difference = num1 - num2;
+
+
+            System.out.println("The difference between " + num1 + " and " + num2 + " is: " + difference + ".");
+        }
+    }
+
+
