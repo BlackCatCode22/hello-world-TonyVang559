3dc580c9bb7b14c8109727f53a0bfa1a88aa1e1e
Tony Vang <97489898+TonyVang559@users.noreply.github.com>
2024-01-19 09:43:48 -0800

    Add files via upload

---
+++ VariablesAndMath.java
@@ -0,0 +1,39 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+public class VariablesAndMath {
+    public static void main(String[] args) {
+
+        System.out.println("\nVariables and Math");
+
+        // Variable and double variable in subtraction for Math operation
+        int myAge  = 25;
+
+        double herAge = 20.5;
+
+        double resultAge = myAge - herAge;
+
+        System.out.println("\nThe difference of " + myAge + " and " + herAge + " is " + resultAge);
+
+
+        // Variables in division for Math operation
+        int cookieLight = 25;
+        int cookieDark = 100;
+
+        int allCookies = cookieDark/cookieLight;
+
+        System.out.println("\nThe quotient of " + cookieDark + " and " + cookieLight + " is " + allCookies);
+
+
+        // Variable in multiplication for Math operation
+        double A = 25;
+        double B = 4;
+
+        double C = A * B;
+
+        System.out.println("\nThe product for " + A + " and " + B + " is " + C);
+
+    }
+
+}
